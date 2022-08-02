3511b2930fe8292bffcd87efed820675771c0074
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-08-02 17:44:14 +0200

    Add permission request

--- app/src/main/java/io/github/breskin/decisionmaker/RecordButton.kt
+++ app/src/main/java/io/github/breskin/decisionmaker/RecordButton.kt
@@ -1,8 +1,12 @@
 package io.github.breskin.decisionmaker
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.content.res.Configuration.UI_MODE_NIGHT_MASK
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -12,24 +16,50 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import io.github.breskin.decisionmaker.ui.theme.DecisionMakerTheme
 
 @Composable
-fun RecordButton(modifier: Modifier = Modifier, animationProgress: Float = 0f, onClick: () -> Unit = {}) {
+fun RecordButton(
+    modifier: Modifier = Modifier,
+    animationProgress: Float = 0f,
+    onClick: () -> Unit = {}
+) {
+    var showPermissionModal by remember {
+        mutableStateOf(false)
+    }
+
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+        if (granted) {
+            onClick()
+        } else {
+            showPermissionModal = true
+        }
+    }
+
     BoxWithConstraints(modifier, contentAlignment = Alignment.Center) {
         val width = maxWidth / 2
         val height = maxHeight / 2
+        val context = LocalContext.current
 
         Box(
             Modifier
@@ -42,7 +72,17 @@ fun RecordButton(modifier: Modifier = Modifier, animationProgress: Float = 0f, o
         Box(
             Modifier
                 .clip(CircleShape)
-                .clickable { onClick() }
+                .clickable {
+                    if (ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.RECORD_AUDIO
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        onClick()
+                    } else {
+                        launcher.launch(Manifest.permission.RECORD_AUDIO)
+                    }
+                }
                 .background(MaterialTheme.colorScheme.primary)
                 .width(width)
                 .height(height),
@@ -55,6 +95,17 @@ fun RecordButton(modifier: Modifier = Modifier, animationProgress: Float = 0f, o
                 modifier = Modifier.fillMaxSize(0.5f)
             )
         }
+    }
+
+    if (showPermissionModal) {
+        AlertDialog(
+            onDismissRequest = { showPermissionModal = false },
+            title = { Text(text = "Wymagane uprawnienie") },
+            text = { Text(text = "Aplikacja wymaga uprawnienia do nagrywania dźwięku") },
+            confirmButton = { Button(onClick = { showPermissionModal = false }) {
+                Text(text = "OK")
+            } },
+        )
     }
 }
 
