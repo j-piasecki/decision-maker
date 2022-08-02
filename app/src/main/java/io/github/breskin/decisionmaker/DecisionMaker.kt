0c6223ab55cbb6ba9ea6a4f0d8ec8a5aad911f16
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-08-02 16:15:34 +0200

    Initial commit

---
+++ app/src/main/java/io/github/breskin/decisionmaker/DecisionMaker.kt
@@ -0,0 +1,8 @@
+package io.github.breskin.decisionmaker
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DecisionMaker() {
+
+}
---
+++ app/src/main/java/io/github/breskin/decisionmaker/MainActivity.kt
@@ -0,0 +1,27 @@
+package io.github.breskin.decisionmaker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import io.github.breskin.decisionmaker.ui.theme.DecisionMakerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DecisionMakerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DecisionMaker()
+                }
+            }
+        }
+    }
+}
